e92ad34c5243511c6157b2c6593c9f10d3b254e6
Hyungkeun.song(Max) <sjjang61@users.noreply.github.com>
2022-12-16 12:08:23 +0900

    [hyungkeun.song] targeting model test code

--- model/TargetingCondition.java
+++ model/TargetingCondition.java
@@ -17,3 +17,27 @@ public class TargetingCondition {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
+
+/*
+    @Test
+    public void test_extParam_json_parser(){
+        //String jsonParam = "{\"sex\" : \"female\",\"age\" : 10, \"os\" : \"android\", \"join_date\" : \"2022-05-16\" }";
+        String jsonParam = "[{ \"type\" : \"sex1\", \"oper\" : \"equal\", \"value\" : \"male\" }, { \"type\" : \"sex2\", \"oper\" : \"equal\", \"value\" : \"male\" } ]";
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+
+            // convert JSON string to Map
+            List<TargetingCondition> targetingList = Arrays.asList(mapper.readValue(jsonParam, TargetingCondition[].class));
+
+            for ( TargetingCondition target : targetingList ){
+                System.out.println( target );
+            }
+
+            //System.out.println( map.get("join_date"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }   
+*/    
